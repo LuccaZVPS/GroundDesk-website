c495cb8376b7f8f22151fc42ed3f59a8162cd5f5
Lucca <luccazvps@gmail.com>
2023-01-05 13:25:44 -0300

    refactor: change hero class name

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header";
 import "./styles.scss";
 export const Hero = () => {
   return (
-    <section>
+    <section className="hero">
       <div className="blue-empty"></div>
       <Header />
       <Content />
